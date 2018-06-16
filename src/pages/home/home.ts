699cf2a3e898dd071b1573483c8ca46f791fad6e
jmitchell0528 <jlm0528@yahoo.com>
2018-06-16 16:12:38 -0500

    First demo app complete

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Geolocation } from '@ionic-native/geolocation';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +8,21 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  coords: any;
+  accuracy: any;
+  error: any;
 
+  constructor(
+    public navCtrl: NavController,
+    private geolocation: Geolocation
+  ) {}
+
+  watch() {
+    this.geolocation.getCurrentPosition().then((resp) => {
+      this.coords = resp.coords.latitude + ' ' + resp.coords.longitude;
+      this.accuracy = resp.coords.accuracy + ' meters';
+    }).catch((error) => {
+      this.error = 'Error getting location: ' + error;
+    })
   }
-
 }
